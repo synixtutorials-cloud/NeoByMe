f95a66dcf09ac0262a27ffed1f70195aeb904d26
synixtutorials-cloud <synixtutorials@gmail.com>
2026-09-20 22:43:18 +0500

    Limit slash commands to Discord limit

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,2 +1,25 @@
-require('dotenv').config();const {REST,Routes}=require('discord.js');const commands=require('./commands');
-(async()=>{const rest=new REST({version:'10'}).setToken(process.env.DISCORD_TOKEN);const route=process.env.GUILD_ID?Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILD_ID):Routes.applicationCommands(process.env.CLIENT_ID);await rest.put(route,{body:commands.map(c=>c.toJSON())});console.log(`Registered ${commands.length} commands.`)})().catch(console.error);
+require('dotenv').config();
+
+const { REST, Routes } = require('discord.js');
+const commands = require('./commands');
+
+(async () => {
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+  const route = process.env.GUILD_ID
+    ? Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      )
+    : Routes.applicationCommands(process.env.CLIENT_ID);
+
+  const commandData = commands
+    .slice(0, 100)
+    .map(command => command.toJSON());
+
+  console.log(`Registering ${commandData.length} of ${commands.length} commands...`);
+
+  await rest.put(route, { body: commandData });
+
+  console.log(`Successfully registered ${commandData.length} commands.`);
+})().catch(console.error);
