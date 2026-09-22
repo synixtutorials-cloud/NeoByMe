7ad6e869ed4388fd40478ea639f6de5def364769
synixtutorials-cloud <synixtutorials@gmail.com>
2026-09-22 18:03:07 +0500

    Fix ticket user channel access

--- src/tickets.js
+++ src/tickets.js
@@ -253,6 +253,13 @@ async function createTicket(interaction, type) {
     ]
   });
 
+  await channel.permissionOverwrites.edit(interaction.user.id, {
+    ViewChannel: true,
+    SendMessages: true,
+    ReadMessageHistory: true,
+    AttachFiles: true
+  });
+
   db.prepare("INSERT INTO tickets (channel_id, guild_id, user_id, type, closed, claimed_by, created_at) VALUES (?, ?, ?, ?, 0, NULL, ?)").run(channel.id, interaction.guild.id, interaction.user.id, type, Date.now());
 
   const embed = new EmbedBuilder()
